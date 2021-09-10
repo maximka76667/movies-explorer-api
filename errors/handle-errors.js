c58137f38b82bcba41c05b573381e058b60c2e16
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-09-10 23:48:28 +0200

    микро-корректировка

--- errors/handle-errors.js
+++ errors/handle-errors.js
@@ -3,9 +3,9 @@ const ConflictError = require('./conflict-error');
 const { errorMessages } = require('./error-config');
 
 const handleErrors = (err) => {
-  if (err.name === 'MongoServerError' && err.code === 11000) { return new ConflictError(errorMessages.conflictErrorMessage); }
+  if (err.name === 'MongoServerError' && err.code === 11000) return new ConflictError(errorMessages.conflictErrorMessage);
   if (err.name === 'CastError') return new BadRequestError(errorMessages.castErrorMessage);
-  if (err.name === 'ValidationError') { return new BadRequestError(errorMessages.validationErrorMessage.default); }
+  if (err.name === 'ValidationError') return new BadRequestError(errorMessages.validationErrorMessage.default);
   return err;
 };
 
